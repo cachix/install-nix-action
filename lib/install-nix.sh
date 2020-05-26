642cfa0200128cfdd6c201f97398c427100891a2
Domen Kožar <domen@dev.si>
2020-05-26 14:34:45 +0200

    Prepare for Nix 2.3.5

--- lib/install-nix.sh
+++ lib/install-nix.sh
@@ -1,46 +1,24 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-export here=$(dirname "${BASH_SOURCE[0]}")
+# Set jobs to number of cores
+sudo sh -c 'echo max-jobs = auto >> /tmp/nix.conf'
+# Allow binary caches for runner user
+sudo sh -c 'echo trusted-users = root runner >> /tmp/nix.conf'
 
-nixConf() {
-  sudo mkdir -p /etc/nix
-  # Workaround a segfault: https://github.com/NixOS/nix/issues/2733
-  sudo sh -c 'echo http2 = false >> /etc/nix/nix.conf'
-  # Set jobs to number of cores
-  sudo sh -c 'echo max-jobs = auto >> /etc/nix/nix.conf'
-  # Allow binary caches for runner user
-  sudo sh -c 'echo trusted-users = root runner >> /etc/nix/nix.conf'
-}
+sh <(curl -L ${INPUT_INSTALL_URL:-https://nixos.org/nix/install}) \
+  --daemon --daemon-user-count 4 --nix-extra-conf-file /tmp/nix.conf --darwin-use-unencrypted-nix-store-volume
 
 if [[ $OSTYPE =~ darwin ]]; then
-  # Catalina workaround https://github.com/NixOS/nix/issues/2925
-  $here/create-darwin-volume.sh
-
   # Disable spotlight indexing of /nix to speed up performance
   sudo mdutil -i off /nix
-fi
 
-nixConf
-
-# Needed due to multi-user being too defensive
-export ALLOW_PREEXISTING_INSTALLATION=1
-
-sh <(curl -L ${INPUT_INSTALL_URL:-https://nixos.org/nix/install}) --daemon
-
-# write nix.conf again as installation overwrites it
-nixConf
-
-# macOS needs certificates hints
-if [[ $OSTYPE =~ darwin ]]; then
+  # macOS needs certificates hints
   cert_file=/nix/var/nix/profiles/default/etc/ssl/certs/ca-bundle.crt
   echo "::set-env name=NIX_SSL_CERT_FILE::$cert_file"
   export NIX_SSL_CERT_FILE=$cert_file
   sudo launchctl setenv NIX_SSL_CERT_FILE "$cert_file"
 fi
-
-# Reload the daemon to pick up changes
-sudo pkill -HUP nix-daemon
 
 # Set paths
 echo "::add-path::/nix/var/nix/profiles/per-user/runner/profile/bin"
--- src/main.ts
+++ src/main.ts
@@ -1,20 +1,3 @@
 import { execFileSync } from 'child_process';
-import { exit } from 'process';
-import { createConnection } from 'net';
-
-async function awaitSocket() {
-  const daemonSocket = createConnection({ path: '/nix/var/nix/daemon-socket/socket' });
-  daemonSocket.on('error', async () => {
-    console.log('Waiting for daemon socket to be available, reconnecting...');
-    await new Promise(resolve => setTimeout(resolve, 500));
-    await awaitSocket();
-  });
-  daemonSocket.on('connect', () => {
-    exit(0);
-  });
-}
 
 execFileSync(`${__dirname}/install-nix.sh`, { stdio: 'inherit' });
-
-// nc doesn't work correctly on macOS :(
-awaitSocket();
