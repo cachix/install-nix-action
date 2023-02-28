90a97c60ab0592602a45d67cfe2ab7b1dd7a3d37
John Murray <5672686+JRMurr@users.noreply.github.com>
2023-02-28 15:55:30 -0500

    export new path

--- install-nix.sh
+++ install-nix.sh
@@ -90,6 +90,10 @@ fi
 echo "/nix/var/nix/profiles/default/bin" >> "$GITHUB_PATH"
 echo "/nix/var/nix/profiles/per-user/$USER/profile/bin" >> "$GITHUB_PATH"
 
+# new path for nix 2.14
+echo "$HOME/.local/state/nix/profile/bin" >> "$GITHUB_PATH"
+
+
 if [[ $INPUT_NIX_PATH != "" ]]; then
   echo "NIX_PATH=${INPUT_NIX_PATH}" >> "$GITHUB_ENV"
 fi
