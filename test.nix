f4aefd48be183fd42c09d44b175998bfd572e2f2
Victor Engmark <vengmark@linz.govt.nz>
2023-03-10 11:52:03 +1300

    refactor: Format Nix file using Alejandra

--- test.nix
+++ test.nix
@@ -1,18 +1,18 @@
 # Realizes <num>> of derivations with size of <size>MB
-{ size ? 1 # MB
-, num ? 10 # count 
-, currentTime ? builtins.currentTime
-, noChroot ? false
+{
+  size ? 1, # MB
+  num ? 10, # count
+  currentTime ? builtins.currentTime,
+  noChroot ? false,
 }:
-
-with import <nixpkgs> {};
-
-let
-  drv = i: runCommand "${toString currentTime}-${toString i}" {
-    __noChroot = noChroot;
-  } ''
-    dd if=/dev/zero of=$out bs=${toString size}MB count=1
-  '';
-in writeText "empty-${toString num}-${toString size}MB" ''
-  ${lib.concatMapStringsSep "" drv (lib.range 1 num)}
-''
+with import <nixpkgs> {}; let
+  drv = i:
+    runCommand "${toString currentTime}-${toString i}" {
+      __noChroot = noChroot;
+    } ''
+      dd if=/dev/zero of=$out bs=${toString size}MB count=1
+    '';
+in
+  writeText "empty-${toString num}-${toString size}MB" ''
+    ${lib.concatMapStringsSep "" drv (lib.range 1 num)}
+  ''
