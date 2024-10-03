4204e15198ce9348dda13b658f2b0b45397cf9e7
Michael Hoang <enzime@users.noreply.github.com>
2024-10-03 12:35:16 +1000

    nix: 2.24.8 -> 2.24.9

--- install-nix.sh
+++ install-nix.sh
@@ -93,7 +93,7 @@ echo "installer options: ${installer_options[*]}"
 
 # There is --retry-on-errors, but only newer curl versions support that
 curl_retries=5
-while ! curl -sS -o "$workdir/install" -v --fail -L "${INPUT_INSTALL_URL:-https://releases.nixos.org/nix/nix-2.24.8/install}"
+while ! curl -sS -o "$workdir/install" -v --fail -L "${INPUT_INSTALL_URL:-https://releases.nixos.org/nix/nix-2.24.9/install}"
 do
   sleep 1
   ((curl_retries--))
