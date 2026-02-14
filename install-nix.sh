eb3d7b01e89bd315de0002620c3840c9edbb8e54
sandydoo <7572407+sandydoo@users.noreply.github.com>
2026-02-14 03:19:18 +0000

    nix: 2.33.0 -> 2.33.3

--- install-nix.sh
+++ install-nix.sh
@@ -102,7 +102,7 @@ echo "installer options: ${installer_options[*]}"
 
 # There is --retry-on-errors, but only newer curl versions support that
 curl_retries=5
-nix_version=2.33.0
+nix_version=2.33.3
 while ! curl -sS -o "$workdir/install" -v --fail -L "${INPUT_INSTALL_URL:-https://releases.nixos.org/nix/nix-${nix_version}/install}"; do
   sleep 1
   ((curl_retries--))
